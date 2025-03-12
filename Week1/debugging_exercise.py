805722bf3a0108dde51b12fb22e383c21fc943ed
sophiebvo <73348689+sophiebvo@users.noreply.github.com>
2025-03-12 01:26:21 +0100

    Created tracker loop and message

--- Week1/debugging_exercise.py
+++ Week1/debugging_exercise.py
@@ -5,8 +5,10 @@ rand = random.randint(1, 20)
 
 print('Guess a number between 1 and 20.')
 guess = int(input())  # number needs to be an integer
+n = 0
 
 while guess != rand:  # if the guess is not equal to the random number, you have to guess again
+    n += 1
     if guess > rand:  # if the guess is too high, tell the user.
         print('Too high. Guess again.')
     else:  # if the guess is too low, tell the user.
@@ -15,4 +17,6 @@ while guess != rand:  # if the guess is not equal to the random number, you have
     print('Enter a new guess: ')
     guess = int(input())
 
+n += 1
 print('You got it! The number was {}'.format(rand))
+print(f'You guessed the correct number in {n} number of guesses.')
